83e4ec4e81374540071b27f947b536dec6b73184
Ricardo Schnur <s9rcschn@stud.uni-saarland.de>
2018-04-26 16:10:07 +0200

    finished problem 2

--- 18ss_prog2/project1_mips_IBAN_calculator/src/moduloStr.s
+++ 18ss_prog2/project1_mips_IBAN_calculator/src/moduloStr.s
@@ -10,5 +10,34 @@
 # Rueckgabewert:
 # v0: Die im Puffer [$a0 bis $a0 + $a1 - 1] darstellte Dezimalzahl (kodiert in ASCII Ziffern '0' bis '9') modulo $a2
 modulo_str:
-	# TODO
+	move 	$s0, $ra		 					# Sichere Rücksprungadresse in $s0
+
+	la 		$t1, 0($a0)
+	addu	$t1, $t1, $a1
+	addiu	$t1, $t1, -1					# $t1 = Zieladresse
+
+	lbu		$v0, 0($a0)						# $v0 = cn in ASCII
+	addiu $v0, $v0, -48					# ASCII zu Ziffer
+	remu 	$v0, $v0, $a2					# $v0 = $v0 mod x
+
+	b 		mod_head							# Sprung nach mod_head
+
+mod_loop:
+	addu	$a0, $a0, 1						# Setze Adresse auf nächsten koeffizienten
+
+	lbu		$t2, 0($a0)						# $t2 = ci in ASCII
+	addiu $t2, $t2, -48					# ASCII zu Ziffer
+	remu 	$t2, $t2, $a2					# $t2 = $t2 mod x
+
+	mulu 	$v0, $v0, 10					# $v0 = 10 * $v0
+	remu 	$v0, $v0, $a2					# $v0 = $v0 mod x
+	addu 	$v0, $v0, $t2					# $v0 = $v0 + ci
+	remu 	$v0, $v0, $a2					# $v0 = $v0 mod x
+
+mod_head:
+	bne 	$a0, $t1, mod_loop		# Abbruchbedingung
+	jr 		$ra
+
+
+	move 	$ra, $s3						  # Lade Rücksprungadresse
 	jr $ra

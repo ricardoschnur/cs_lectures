aeec6003cec0e542c74a48eff2337e864ad93388
Ricardo Schnur <rschnur94@gmail.com>
2018-05-20 10:28:22 +0200

    Started work on graph API

---
+++ 18ss_prog2/project3_c_pagerank/src/graph.c
@@ -0,0 +1,287 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "graph.h"
+
+/*
+  -------------------------------------------------------------------------
+  Types and structs
+  -------------------------------------------------------------------------
+*/
+
+/*
+  Linked list containing the nodes to which there is an edge
+*/
+
+typedef struct edge {
+  unsigned destination;        // position of destination
+  struct edge *next;           // pointer to next edge
+} edge_t;
+
+
+/*
+  a node of a directed graph
+*/
+
+typedef struct node {
+  char label[257];          // name of the node (max. 256 characters)
+  unsigned position;        // nodes are ordered
+  unsigned indeg;           // in-degree of the node
+  unsigned outdeg;          // out-degree of the node
+  edge_t *edges;            // outgoing edges
+  struct node *next;        // pointer to next node
+} node_t;
+
+
+/*
+  struct for directed graph
+*/
+
+typedef struct graph {
+  char label[257];
+  unsigned nodecount;   // number of vertices
+  unsigned edgecount;   // number of edges
+  node_t *head;         // pointer to first vertex
+} graph_t;
+
+
+/*
+  -------------------------------------------------------------------------
+  Static functions
+  -------------------------------------------------------------------------
+*/
+
+//  Initialize a node with name label and increase nodecount of G by 1
+static node_t *nodeInit(graph_t *G, const char* label) {
+  node_t *V = malloc( sizeof(V[0]) );  // allocate space for the node
+
+  // Copy label
+  int i = 0;
+  while (label[i] != '\0' && i < 256) {
+    (V->label)[i] = label[i];
+    ++i;
+  }
+  // Check if label longer than 256 characters
+  if (i == 256 && label[i] != '\0') {
+    fprintf(stderr, "Error: Label of node exceeds ");
+    fprintf(stderr, "maximal number of characters (256)\n");
+    exit(1);                 // Stop program with error code 1
+  }
+  (V->label)[i] = '\0';
+
+  V->position = G->nodecount;
+  V->indeg = 0;
+  V->outdeg = 0;
+  V->next = NULL;
+  V->edges = NULL;
+
+  G->nodecount += 1;   // increase nodecount of G
+
+  return V;
+}
+
+
+//  Initialize edge E from V to W
+static void edgeInit(graph_t *G, node_t *V, node_t *W) {
+  edge_t *E = malloc( sizeof(E[0]) );  // allocate space for the node
+
+  E->destination = W->position;        // edge points to W
+  E->next = NULL;                      // no next edge
+
+  G->edgecount += 1;
+  V->outdeg += 1;
+  W->indeg += 1;
+
+  // Find last existing edge in V
+  if (V->edges == NULL) {
+    V->edges = E;
+    return;
+  }
+  else {
+    edge_t *p = V->edges;
+    if (p != NULL) {
+      edge_t *q = p->next;
+      while (q != NULL) {
+        p = q;
+        q = q->next;
+      }
+    }
+    p->next = E;
+  }
+
+}
+
+
+// Destroy edge_t and free memory
+static void edgeDestroy(edge_t *V){
+  if (V != NULL) {
+    edge_t *p = V->next;
+    edgeDestroy(p);
+  }
+
+  free(V);
+}
+
+
+// Destroy node_t and free memory
+static void nodeDestroy(node_t *V){
+  if (V != NULL) {
+    edgeDestroy(V->edges);
+    nodeDestroy(V->next);
+  }
+
+  free(V);
+}
+
+
+/*
+  -------------------------------------------------------------------------
+  Global functions
+  -------------------------------------------------------------------------
+*/
+
+graph_t *graphInit(const char* label) {
+  graph_t *G = malloc( sizeof(G[0]) );  // allocate space for the graph
+  G->nodecount = 0;                     // has no nodes
+  G->edgecount = 0;                     // has no edges
+  G->head = NULL;                       // no first node
+
+  // Set label of G to label
+  int i = 0;
+  while (label[i] != '\0' && i < 256) {
+    (G->label)[i] = label[i];
+    ++i;
+  }
+  // Check if label longer than 256 characters
+  if (i == 256 && label[i] != '\0') {
+    fprintf(stderr, "Error: Label of node exceeds ");
+    fprintf(stderr, "maximal number of characters (256)\n");
+    exit(1);                 // Stop program with error code 1
+  }
+  (G->label)[i] = '\0';
+
+  return G;
+}
+
+
+void graphDestroy(graph_t *G){
+  if (G != NULL) {
+    nodeDestroy(G->head);
+  }
+
+  free(G);
+}
+
+
+void addNode(graph_t *G, const char* label) {
+  // If G is empty add node and return
+  if (G->nodecount == 0) {
+    G->head = nodeInit(G, label);
+    return;
+  }
+
+  /*
+    If G is not empty, check if the node already exists.
+    In case it does exist, return without doing anything.
+  */
+  node_t *V = G->head;  // V points to the first node of G
+  if ( strcmp(label, V->label) == 0 ) {
+    return;
+  }
+
+  for (unsigned i = 1; i < G->nodecount ; ++i) {
+    V = V->next;        // Change pointer to next node
+
+    // Compare labels
+    if ( strcmp(label, V->label) == 0 ) {
+      return;
+    }
+  }
+
+  // V now points to the last node in G
+  V->next = nodeInit(G, label);
+}
+
+
+void stats(graph_t *G) {
+  unsigned minin, maxin, minout, maxout;
+  node_t *V = G->head;
+
+  minin = V->indeg;
+  maxin = V->indeg;
+  minout = V->outdeg;
+  maxout = V->outdeg;
+
+  for (unsigned i = 1; i < G->nodecount; ++i) {
+    V = V->next;
+
+    if(V->indeg < minin) {
+      minin = V->indeg;
+    }
+
+    if(V->indeg > maxin) {
+      maxin = V->indeg;
+    }
+
+    if(V->outdeg < minout) {
+      minout = V->outdeg;
+    }
+
+    if(V->outdeg > maxout) {
+      maxout = V->outdeg;
+    }
+  }
+
+
+  printf("%s:\n", G->label);
+  printf("- num nodes: %u\n", G->nodecount);
+  printf("- num edges: %u\n", G->edgecount);
+  printf("- indegree: %u-%u\n", minin, maxin);
+  printf("- outdegree: %u-%u\n", minout, maxout);
+}
+
+
+void addEdge(graph_t *G, const char* origin, const char* destination){
+  // If they do not exist, add nodes to graph
+  addNode(G, origin);
+  addNode(G, destination);
+
+  // Find origin
+  node_t *V = G->head;   // V points to the first node of G
+  while (strcmp(origin, V->label) != 0) {
+    V = V->next;
+  }
+
+  // Find destination
+  node_t *W = G->head;   // W points to the first node of G
+  while (strcmp(destination, W->label) != 0) {
+    W = W->next;
+  }
+
+  edgeInit(G, V, W);
+  printf(" V->edges->destination: %d\n",  V->edges->destination);
+  printf(" V->edges->next == NULL: %d\n",  V->edges->next == NULL);
+  return;
+}
+
+
+void graphPrint(graph_t *G){
+  printf("digraph %s {\n", G->label);
+
+  node_t *V = G->head;
+  while (V != NULL) {
+    edge_t *E = V->edges;
+    while (E != NULL) {
+      node_t *W = G->head;
+      for (unsigned i = 0; i < E->destination; ++i) {
+        W = W->next;
+      }
+      printf("%s -> %s;\n", V->label, W->label);
+      E = E->next;
+    }
+    V = V->next;
+  }
+
+  printf("}\n");
+}
---
+++ 18ss_prog2/project3_c_pagerank/src/graph.h
@@ -0,0 +1,44 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+/*
+  Datatype for directed graphs
+  Note that parallel edges and self-edges are allowed!
+*/
+typedef struct graph graph_t;
+
+
+/*
+  Initialize an empty graph G.
+  You are responsible to free memory with graphDestroy(*G)!
+*/
+graph_t *graphInit();
+
+
+// Destroy graph and free memory
+void graphDestroy(graph_t *G);
+
+
+// prints the graph
+void graphPrint(graph_t *G);
+
+
+// Add a node with name label to the graph G
+void addNode(graph_t *G, const char* label);
+
+
+/*
+  Add an edge from origin to destination to G.
+  If either of these nodes does not exist, add them.
+*/
+void addEdge(graph_t *G, const char* origin, const char* destination);
+
+
+/*
+  Print some details of the graph:
+  number of nodes, number of edges, minimal and maximal in-/ and out-degrees
+*/
+void stats(graph_t *G);
+
+
+#endif
